eb2cb770f17f7e38208ff523898b58202510c1fd
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-06 10:06:08 +0200

    project finished

---
+++ script.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('#modal');
+const openModalBtn = document.querySelector('#open-modal-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const overlay = document.querySelector('#overlay');
+
+openModalBtn.addEventListener('click', () =>
+{
+    modal.classList.add('open')//add a class open to model
+    overlay.classList.add('open')
+})
+
+closeModalBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+function closeModal()
+{
+    modal.classList.remove('open')
+    overlay.classList.remove('open')
+}
